0e348c3e4d1c970e3fc13ebdb21c80d981e84658
Sayali <sayali.undale_comp21@pccoer.in>
2023-06-14 12:04:30 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from 'react';
+import "./App.css"
+
+const App =() =>{
+  
+  const color = "yellow";
+  const name = "I am Yellow";
+  const [bg, setBg] = useState(color);
+  const [Name, setName] = useState (name);
+
+  const myFunction = () =>{
+    let newBg = "purple";
+    let newName = "I am Purple";
+    setBg(newBg);
+    setName(newName);
+
+  }
+  const backColor =() =>{
+    
+    setBg(color);
+    setName(name);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div style= {{backgroundColor:bg}}>
+    <button onMouseEnter ={myFunction} onMouseLeave={backColor}>{Name}</button>
     </div>
-  );
+      
+    </>
+  )
+
 }
 
-export default App;
+export default App
